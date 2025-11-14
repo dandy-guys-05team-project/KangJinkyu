0929315a259a7aa65a3799a524bebc82c46f4395
kajkyu <kajkyu@naver.com>
2025-11-14 13:15:54 +0900

    java 실습

--- main/java/com/mysite/sbb/question/QuestionService.java
+++ main/java/com/mysite/sbb/question/QuestionService.java
@@ -6,6 +6,10 @@ import java.time.LocalDateTime;
 
 import com.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -15,6 +19,11 @@ public class QuestionService {
 	
 	public List<Question> getList(){
 		return this.questionRepository.findAll();
+	}
+	
+	public Page<Question> getList(int page) {
+		Pageable pageable = PageRequest.of(page, 10);
+		return this.questionRepository.findAll(pageable);
 	}
 	
 	public Question getQuestion(Integer id) {
